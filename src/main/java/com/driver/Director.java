5a6eb02a03199ddba430480daf7ff823bd491c70
Awais Raees Ansari <80111573+AnsariAwaisRaees@users.noreply.github.com>
2023-08-28 14:24:00 +0530

    Update Director.java

--- src/main/java/com/driver/Director.java
+++ src/main/java/com/driver/Director.java
@@ -5,33 +5,38 @@ public class Director {
     private int numberOfMovies;
     private double imdbRating;
 
+    //---------- Constructor ---------
     public Director() {
     }
-
     public Director(String name, int numberOfMovies, double imdbRating) {
         this.name = name;
         this.numberOfMovies = numberOfMovies;
         this.imdbRating = imdbRating;
     }
 
+    //------------ Getter -----------
+
+
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public int getNumberOfMovies() {
         return numberOfMovies;
     }
 
-    public void setNumberOfMovies(int numberOfMovies) {
-        this.numberOfMovies = numberOfMovies;
-    }
-
     public double getImdbRating() {
         return imdbRating;
+    }
+
+    //-------------- Setter -------------
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setNumberOfMovies(int numberOfMovies) {
+        this.numberOfMovies = numberOfMovies;
     }
 
     public void setImdbRating(double imdbRating) {
